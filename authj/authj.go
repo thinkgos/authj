0a39bbe4a0cce2e9bfb74d244423d0048f5dfebb
mo <thinkgo@aliyun.com>
2020-05-15 13:36:23 +0800

    fix authj bug

--- authj/authj.go
+++ authj/authj.go
@@ -1,22 +1,53 @@
 package authj
 
 import (
+	"context"
+	"net/http"
+
 	"github.com/casbin/casbin/v2"
 	"github.com/gin-gonic/gin"
-	"github.com/thinkgos/http-middlewares/authj"
-
-	"github.com/thinkgos/gin-middlewares/wrap"
 )
+
+// contextKey is a value for use with context.WithValue. It's used as
+// a pointer so it fits in an interface{} without allocation. This technique
+// for defining context keys was copied from Go 1.7's new use of context in net/http.
+type ctxAuthKey struct{}
 
 // NewAuthorizer returns the authorizer
 // uses a Casbin enforcer and Subject function as input
 func NewAuthorizer(e *casbin.Enforcer) gin.HandlerFunc {
-	return wrap.HTTPf(authj.NewAuthorizer(e))
+	return func(c *gin.Context) {
+		//checks the userName,path,method permission combination from the request.
+		allowed, err := e.Enforce(subject(c), c.Request.URL.Path, c.Request.Method)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"code":    http.StatusInternalServerError,
+				"message": "Permission validation errors occur!",
+			})
+			c.Abort()
+			return
+		} else if !allowed {
+			// the 403 Forbidden to the client
+			c.JSON(http.StatusForbidden, gin.H{
+				"code":    http.StatusForbidden,
+				"message": "Permission denied!",
+			})
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
+
+// subject returns the value associated with this context for subjectCtxKey,
+func subject(c *gin.Context) string {
+	val, _ := c.Request.Context().Value(ctxAuthKey{}).(string)
+	return val
 }
 
 // ContextWithSubject return a copy of parent in which the value associated with
 // subjectCtxKey is subject.
 func ContextWithSubject(c *gin.Context, subject string) {
-	ctx := authj.ContextWithSubject(c.Request.Context(), subject)
+	ctx := context.WithValue(c.Request.Context(), ctxAuthKey{}, subject)
 	c.Request = c.Request.WithContext(ctx)
 }
