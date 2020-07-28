a24068e767e43042a2ba2fd8a4e1ca033f1021ff
mo <thinkgo@aliyun.com>
2020-07-28 12:23:01 +0800

    user simple c.AbortWithStatusJSON

--- authj/authj.go
+++ authj/authj.go
@@ -20,19 +20,17 @@ func NewAuthorizer(e *casbin.Enforcer) gin.HandlerFunc {
 		//checks the userName,path,method permission combination from the request.
 		allowed, err := e.Enforce(subject(c), c.Request.URL.Path, c.Request.Method)
 		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
 				"code":    http.StatusInternalServerError,
 				"message": "Permission validation errors occur!",
 			})
-			c.Abort()
 			return
 		} else if !allowed {
 			// the 403 Forbidden to the client
-			c.JSON(http.StatusForbidden, gin.H{
+			c.AbortWithStatusJSON(http.StatusForbidden, gin.H{
 				"code":    http.StatusForbidden,
 				"message": "Permission denied!",
 			})
-			c.Abort()
 			return
 		}
 		c.Next()
