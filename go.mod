bfcab9465b3cbda13b7141bad1162df332cf783e
thinkgo <thinkgo@aliyun.com>
2021-01-14 15:46:03 +0800

    update

--- go.mod
+++ go.mod
@@ -8,13 +8,12 @@ require (
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
 	github.com/golang/protobuf v1.4.3 // indirect
-	github.com/google/uuid v1.1.4 // indirect
 	github.com/json-iterator/go v1.1.10 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
-	github.com/thinkgos/http-middlewares v0.0.6
+	github.com/thinkgos/http-middlewares v0.1.0
 	github.com/ugorji/go v1.2.3 // indirect
 	go.uber.org/zap v1.16.0
 	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
--- nocache/nocache_test.go
+++ nocache/nocache_test.go
@@ -13,17 +13,18 @@ func Test_NoCacheHeaders(t *testing.T) {
 	responseHeaders := map[string]string{
 		"Cache-Control": "no-cache, no-store, no-transform, must-revalidate, private, max-age=0",
 		"Pragma":        "no-cache",
-		"Expires":       time.Unix(0, 0).Format(time.RFC1123),
+		"Expires":       time.Unix(0, 0).UTC().Format(http.TimeFormat),
 	}
-	recorder := httptest.NewRecorder()
-	m := gin.New()
 
+	m := gin.New()
 	m.Use(NoCache())
+
+	recorder := httptest.NewRecorder()
 	r, _ := http.NewRequest("GET", "/", nil)
 	m.ServeHTTP(recorder, r)
 
 	for key, value := range responseHeaders {
-		if recorder.HeaderMap[key][0] != value {
+		if recorder.Header()[key][0] != value {
 			t.Errorf("Missing header: %s", key)
 		}
 	}
