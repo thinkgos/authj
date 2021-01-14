2a311ddbfc77cd0df1b51c06c62bec9c0b0ca26b
thinkgo <thinkgo@aliyun.com>
2021-01-14 15:33:24 +0800

    add no cache test

---
+++ nocache/nocache_test.go
@@ -0,0 +1,30 @@
+package nocache
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Test_NoCacheHeaders(t *testing.T) {
+	responseHeaders := map[string]string{
+		"Cache-Control": "no-cache, no-store, no-transform, must-revalidate, private, max-age=0",
+		"Pragma":        "no-cache",
+		"Expires":       time.Unix(0, 0).Format(time.RFC1123),
+	}
+	recorder := httptest.NewRecorder()
+	m := gin.New()
+
+	m.Use(NoCache())
+	r, _ := http.NewRequest("GET", "/", nil)
+	m.ServeHTTP(recorder, r)
+
+	for key, value := range responseHeaders {
+		if recorder.HeaderMap[key][0] != value {
+			t.Errorf("Missing header: %s", key)
+		}
+	}
+}
